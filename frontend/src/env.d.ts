b32947c8cd8c9aa741c2f666054c1369997cffde
Kirisame <gensky377@gmail.com>
2025-03-19 23:00:31 +0900

    chore: optimized config

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="@rsbuild/core/types" />
+
+declare module "*.css" {
+  const classes: CSSModuleClasses;
+  export default classes;
+}
+
+declare module "*.sass" {
+  const classes: CSSModuleClasses;
+  export default classes;
+}
+
+declare module "*.scss" {
+  const classes: CSSModuleClasses;
+  export default classes;
+}
