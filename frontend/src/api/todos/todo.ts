c91520c74c0dceebf758629d656700238ba0ab32
Binwalker <gensky377@gmail.com>
2025-08-28 21:36:41 +0900

    fix: change server port

--- frontend/src/api/todos/todo.ts
+++ frontend/src/api/todos/todo.ts
@@ -6,7 +6,7 @@ import {
   type PaginationParams,
 } from "./types";
 
-const createApi = (baseURL: string = "http://localhost:8333"): AxiosInstance =>
+const createApi = (baseURL: string = "http://localhost:3001"): AxiosInstance =>
   axios.create({
     baseURL,
     timeout: 10000,
