731804cd301757ae9c5dfbfa6de21ee0304af31d
Binwalker <gensky377@gmail.com>
2025-10-31 20:05:57 +0900

    lint: fix

--- server/src/main.rs
+++ server/src/main.rs
@@ -74,9 +74,7 @@ async fn main() {
         )
         .with_state(db);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
